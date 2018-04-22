b053034c445142bed13984941143096a3dcb91af
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-04-22 09:43:31 +0200

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,31 +1,18 @@
-
 package ru.job4j;
-/**
-* Package for calculate task.
-*
-* @author Sergey Belyj (sergiuszbelyj@gmail.com).
-* @version $Id$.
-* @since 0.1.
-*/
 
+/**
+ * Calculate.
+ *
+ * @author Sergey Belyj (sergiuszbelyj@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
-
-
-/**
+    /**
      * Main.
      * @param args - args.
      */
-	public static void main(String[] args) {
-		Calculate calc = new Calculate();
-		System.out.println(calc.echo("ahh"));
-	}
-/**
-* Method echo.
-* @param name Your name.
-* @return Echo plus your name.
-*/
-	public String echo(String name) {
-  	  return "Echo, echo, echo : " + name;
-}
-	
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,22 +7,29 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Petr Arsentev (parsentev@yandex.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Sergey Belyj (sergiuszbelyj@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/ @Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Petr Arsentev";
-    String expect = "Echo, echo, echo : Petr Arsentev"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenAddOneToOneThenTwo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(
+                out.toString(),
+                is(
+                        String.format(
+                                "Hello World%s",
+                                System.getProperty("line.separator")
+                        )
+                )
+        );
+    }
 }
