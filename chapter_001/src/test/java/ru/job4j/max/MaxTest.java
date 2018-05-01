fb9e2eeb70adc48c12a8860ec0e46235165df51c
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-05-01 18:10:11 +0200

    3.2. Максимум из двух чисел

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -18,4 +18,10 @@ public class MaxTest {
         int result = maxim.max(1, 2);
         assertThat(result, is(2));
     }
+    @Test
+    public void whenSecondLessFirst() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 1);
+        assertThat(result, is(2));
+    }
 }
