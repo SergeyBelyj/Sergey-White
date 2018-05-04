fac8ebd5a3d8c09a030b27053e5c745c3f7aee5f
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-05-04 18:08:46 +0200

    5.5. Слова начинается с ...

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        int k = 0;
+        char[] value = prefix.toCharArray();
+        for (int i = 0; i < value.length; i++) {
+            if (data[i] == value [i]) {
+                k++;
+            }
+        }
+        if (k == value.length ) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        return result;
+    }
+}
