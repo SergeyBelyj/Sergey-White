61596729e3243938707fd1b5a5a89173d6a1663a
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-05-06 16:41:23 +0200

    4.3. Построить шахматную доску в псевдографике.

--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -13,7 +13,7 @@ public class BoardTest {
     public void when5x5Then5x5() {
 
         String result = w.paint(5, 5);
-        String expected = "X X X \\r\\n X X X\\r\\nX X X \\r\\n X X X\\r\\nX X X \\r\\n X X X\\r\\n";
+        String expected = "X X X \r\n X X X\r\nX X X \r\n X X X\r\nX X X \r\n X X X\r\n";
         assertThat(result, is(expected));
     }
 
@@ -21,7 +21,7 @@ public class BoardTest {
     public void when3x3Then3x3() {
 
         String result = w.paint(3, 3);
-        String expected = "X X \\r\\n X X\\r\\nX X \\r\\n X X\\r\\n";
+        String expected = "X X \r\n X X\r\nX X \r\n X X\r\n";
 
         assertThat(result, is(expected));
 
