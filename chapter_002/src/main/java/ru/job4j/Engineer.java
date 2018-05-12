14a103aac1c3a0a2ac1fc6ada1847eeeecff0cc3
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-05-12 11:57:38 +0200

    1. Создать классы - профессий.

--- chapter_002/src/main/java/ru/job4j/Engineer.java
+++ chapter_002/src/main/java/ru/job4j/Engineer.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
-public class Engineer extends Profession{
-    public void build (House home) {
+public class Engineer extends Profession {
+    public void build(House home) {
 
     }
 }
--- chapter_002/src/main/java/ru/job4j/Teacher.java
+++ chapter_002/src/main/java/ru/job4j/Teacher.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
-public class Teacher extends Profession{
-    public void teach (Student name) {
+public class Teacher extends Profession {
+    public void teach(Student name) {
 
     }
 }
