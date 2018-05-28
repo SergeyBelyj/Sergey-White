c9b791512d2f00382446700e20d7f5f4f71308dc
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-05-28 18:23:32 +0200

    5. Рефакторинг теста @Before @After

--- chapter_002/src/test/java/ru/job4j/draw/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/draw/PaintTest.java
@@ -1,75 +1,10 @@
 package ru.job4j.draw;
 
-import org.junit.Test;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
-public class PaintTest {
-    @Test
-    public void whenDrawSquare() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
-        new Paint().draw(new Square());
-        // проверяем результат вычисления
-        assertThat(
-                new String(out.toByteArray()),
-                is(
-                        new StringBuilder()
-                                .append("++++")
-                                .append("+     +")
-                                .append("+     +")
-                                .append("++++")
-                                .append(System.lineSeparator())
-                                .toString()
-                )
-        );
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
-    }
-    @Test
-    public void whenDrawTriangle() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
-        new Paint().draw(new Triangle());
-        // проверяем результат вычисления
-        assertThat(
-                new String(out.toByteArray()),
-                is(
-                        new StringBuilder()
-                                .append("    +   ")
-                                .append("  +   +  ")
-                                .append(" +     + ")
-                                .append("+++++++++")
-                                .append(System.lineSeparator())
-                                .toString()
-                )
-        );
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
-    }
-}
-
-/*
-package ru.job4j.draw;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -114,17 +49,15 @@ public class PaintTest {
         assertThat(
                 new String(this.out.toByteArray()),
                 is(
-                        new StringJoiner(
-                                System.lineSeparator(), "",
-                                System.lineSeparator())
+                        new StringBuilder()
                                 .append("    +   ")
                                 .append("  +   +  ")
                                 .append(" +     + ")
                                 .append("+++++++++")
+                                .append(System.lineSeparator())
                                 .toString()
                 )
         );
     }
 }
 
- */
