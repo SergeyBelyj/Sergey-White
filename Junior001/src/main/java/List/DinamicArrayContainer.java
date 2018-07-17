2f3476e7e0773e12cbf57c3794cdc491972f7979
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-07-17 18:18:49 +0200

    5.3.1. Создать динамический список на базе массива.

---
+++ Junior001/src/main/java/List/DinamicArrayContainer.java
@@ -0,0 +1,31 @@
+package List;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+import static java.lang.System.arraycopy;
+
+public class DinamicArrayContainer<E> implements Iterable<E> {
+
+    Object[] container;
+    public DinamicArrayContainer(Object[] container) {
+        this.container = container;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+     E add(Object[] value) {
+         Object[] list = new Object[container.length + 1];
+         arraycopy(value, 0, list, 0, value.length);
+         arraycopy(container, 0, list, 1, container.length);
+         this.container = list;
+    return null;
+    }
+    E get(int index) {
+        return (E) this.container[index];
+    }
+
+}
