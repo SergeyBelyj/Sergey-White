6d8dd0da26dea09d2e321e7d0d5259493c9bd185
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-07-17 14:01:06 +0200

    5.1.4.  convert(Iterator<Iterator>)

---
+++ Junior001/src/main/java/Iterator/Converter.java
@@ -0,0 +1,23 @@
+package Iterator;
+
+import javax.print.attribute.IntegerSyntax;
+import java.util.Iterator;
+
+public class Converter {
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+
+
+
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return it.next().hasNext();
+            }
+
+            @Override
+            public Integer next() {
+                return it.next().next();
+            }
+        };
+    }
+}
