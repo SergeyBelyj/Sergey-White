96b39e200b7b5ad7f0c301c542aecc53c6f6329d
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-07-17 16:58:08 +0200

    5.3.0. Make method for a simply connected list SimpleArrayList<E>

---
+++ Junior001/src/main/java/List/SimpleArrayList.java
@@ -0,0 +1,40 @@
+package List;
+
+public class SimpleArrayList<E> {
+    private int size;
+    private Node<E> first;
+
+    public void add(E date) {
+        Node<E> newLink = new Node<E>(date);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+
+    public E delete() {
+        this.size--;
+
+            this.first.date = this.first.next.date;
+            this.first.next = this.first.next.next;
+
+        return null;
+    }
+
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.date;
+    }
+    public int getSize() {
+        return this.size;
+    }
+    private static class Node<E> {
+        E date;
+        Node<E> next;
+        Node(E date) {
+            this.date = date;
+        }
+    }
+}
