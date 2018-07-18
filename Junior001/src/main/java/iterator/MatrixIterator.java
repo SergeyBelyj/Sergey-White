41781c7cdc693fd2e93775d9ed3d6210280b0707
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-07-18 17:51:00 +0200

    5.1.1. Итератор для двухмерного массива int[][]

--- Junior001/src/main/java/iterator/MatrixIterator.java
+++ Junior001/src/main/java/iterator/MatrixIterator.java
@@ -1,6 +1,6 @@
 package iterator;
 
-import java.util.Arrays;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -11,57 +11,33 @@ public class MatrixIterator implements Iterator {
         private int  cols = 0;
         private int rows = 0;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof MatrixIterator)) return false;
-        MatrixIterator that = (MatrixIterator) o;
-        return Arrays.equals(values, that.values);
-    }
-
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(values);
-    }
-
     public MatrixIterator(final int[][] values) {
             this.values = values;
-
-        }
-
-        public boolean isNull(int[][] val) {
-            boolean res = false;
-            try {
-                if (val[0].length == 0) {
-                }
-            } catch (ArrayIndexOutOfBoundsException e) {
-                res = true;
-            }
-            return res;
         }
 
         @Override
         public boolean hasNext() {
-            boolean res = isNull(values);
-            return ((values.length == cols + 1) && (values[cols].length == rows) || (res)) ? false : true;
+            return values.length > rows &&  values[rows].length > cols;
         }
 
 
         @Override
         public Object next() {
+            int result;
             hasNext();
             if (!hasNext()) {
                 throw new NoSuchElementException();
-            } else {
-                if (values[cols].length > rows) {
-                    return values[cols][rows++];
-                } else {
-                    rows = 0;
-                    cols++;
-                    return values[cols][rows++];
-
-                }
             }
+            result = values[rows][cols];
+            cols++;
+                if (cols >= values[rows].length) {
+                    cols = 0;
+                    rows++;
+                }
+            return result;
         }
-    }
+}
+
+
+
 
