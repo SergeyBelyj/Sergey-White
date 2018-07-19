26c3bb7a93039346a809253207cd8cc54242c415
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-07-19 15:22:26 +0200

    2. Не перекрывать equals hashCode

---
+++ Junior001/src/test/java/map/UserTest.java
@@ -0,0 +1,20 @@
+package map;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class UserTest {
+    @Test
+    public void makeTwoObjectsOfUser() {
+        User first = new User("Sergey", 0,  25);
+        User second = new User("Sergey", 0, 25);
+        Map<User, Object> map = new HashMap<>();
+        map.put(first,"first");
+        map.put(second, "second");
+        System.out.println(map);
+    }
+}
