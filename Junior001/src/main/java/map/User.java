1eb106bc071386b85403930992dc21b557de8087
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-07-19 16:06:43 +0200

    4. Переопределить только equals

--- Junior001/src/main/java/map/User.java
+++ Junior001/src/main/java/map/User.java
@@ -19,7 +19,16 @@ public class User {
 
 
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (children != user.children) return false;
+        if (birthday != user.birthday) return false;
+        return name != null ? name.equals(user.name) : user.name == null;
     }
+
+
 }
