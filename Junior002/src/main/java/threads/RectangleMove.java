75d99384f18be838c42259f06e09d3a737dbd86c
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-07-21 16:07:16 +0200

    1. Пин-понг.

---
+++ Junior002/src/main/java/threads/RectangleMove.java
@@ -0,0 +1,26 @@
+package threads;
+
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+    private final Rectangle rect;
+
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+
+    public void run() {
+        while (true) {
+           this.rect.setX(this.rect.getX() + 1);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
---
+++ Junior002/src/main/java/threads/Runnable.java
@@ -0,0 +1,6 @@
+package threads;
+
+public interface Runnable {
+
+
+}
