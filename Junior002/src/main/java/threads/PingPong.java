6a23396e656ee8c9254e90b89edc6ca7d1f03b87
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-07-23 16:55:44 +0200

    1. Пин-понг.

--- Junior002/src/main/java/threads/PingPong.java
+++ Junior002/src/main/java/threads/PingPong.java
@@ -1,8 +1,5 @@
 package threads;
 
-
-
-
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -12,27 +9,18 @@ import javafx.stage.Stage;
 public class PingPong extends Application {
     private static final String JOB4J = "Пинг-понг www.job4j.ru";
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage stage) {
         int limitX = 300;
         int limitY = 300;
         Group group = new Group();
-        Rectangle rect = new Rectangle(0, 100, 5, 5);
+        Rectangle rect = new Rectangle(50, 100, 10, 10);
         group.getChildren().add(rect);
-        Thread mainThread = new Thread(new RectangleMove(rect));
-        mainThread.start();
+         new Thread(new RectangleMove(rect)).start();
         stage.setScene(new Scene(group, limitX, limitY));
         stage.setTitle(JOB4J);
-        stage.setResizable(true);
+        stage.setResizable(false);
         stage.show();
-        stage.setOnCloseRequest(
-                event -> {
-                    mainThread.interrupt();
-                });
     }
 }
 
