eb71a41921b11a76b7ff8d5af5a1ba577ffda802
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-07-23 17:07:48 +0200

    5.2.1. Реализовать SimpleArray<T>

--- Junior001/src/main/java/generic/SimpleList.java
+++ Junior001/src/main/java/generic/SimpleList.java
@@ -2,15 +2,12 @@ package generic;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 import static java.lang.System.arraycopy;
-import static java.lang.System.in;
 
 public class SimpleList<T> implements Iterable<T> {
 
-
-    Object[] objects;
-    int index = 0;
+    private Object[] objects;
+    private int index = 0;
     private int iterIndex = 0;
 
     public SimpleList(int size) {
@@ -32,21 +29,15 @@ public class SimpleList<T> implements Iterable<T> {
         arraycopy(this.objects, ind + 1, news, ind, this.objects.length - ind - 1);
         this.objects = news;
         index--;
-
     }
-
 
     @Override
     public Iterator<T> iterator() {
         Iterator<T> temp = new Iterator<T>() {
-
-
-
             @Override
             public boolean hasNext() {
                 return iterIndex < index && objects[iterIndex] != null;
             }
-
             @Override
             public T next() {
                 if (!hasNext()) {
