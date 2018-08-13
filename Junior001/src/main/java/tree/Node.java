8f489b48d205c60f40f6b84554e4c6ceecd0cf67
SergeyBelyj <sergiuszbelyj@gmail.com>
2018-08-13 14:51:42 +0200

    1. Создать элементарную структуру дерева

--- Junior001/src/main/java/tree/Node.java
+++ Junior001/src/main/java/tree/Node.java
@@ -21,27 +21,6 @@ public class Node<E extends Comparable<E>>  {
     }
 
     public boolean eqValue(E that) {
-        return this.value.compareTo(that) == 0;
-    }
-
-    public Optional<Node<E>> findBy(E value) {
-        Optional<Node<E>> rsl = Optional.empty();
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (el.eqValue(value)) {
-                rsl = Optional.of(el);
-                break;
-            }
-            for (Node<E> child : el.leaves()) {
-                data.offer(child);
-            }
-        }
-        return rsl;
-    }
-
-
-
+        return this.value.compareTo(that) == 0;  }
 
 }
--- Junior001/src/main/java/tree/Tree.java
+++ Junior001/src/main/java/tree/Tree.java
@@ -3,33 +3,19 @@ package tree;
 import java.util.*;
 
 public class Tree<E extends Comparable<E>> implements Iterator<E> {
-    private Node<E> root;
+    private Node<E> parent;
+    private Optional<Node<E>> child;
 
     public Tree(E rootData) {
-        root = new Node<E>(rootData);
+        parent = new Node<E>(rootData);
     }
 
     public void add(Node<E> parent, Node<E> child) {
-
-        if (root.value == parent.value) {
-            root.addChild(child);
-        }
-        else {
-            Queue<Node<E>> data = new LinkedList<>();
-            data.offer(this.root);
-            while (!data.isEmpty()) {
-                Node<E> el = data.poll();
-                for (Node<E> child2 : el.leaves()) {
-                    if (child2.value == parent.value) {
-                        child2.children.add(child);
-                        data.offer(child2);
-                        break;
-                    } else  data.offer(child2);
-                }
-            }
-            }
-        }
-
+        if (this.parent.value == parent.value) {
+            this.parent.children.add(child);
+        } else { this.child = findBy(parent.value);
+        this.child.get().children.add(child); }
+    }
 
     @Override
     public boolean hasNext() {
@@ -44,7 +30,7 @@ public class Tree<E extends Comparable<E>> implements Iterator<E> {
     public Optional<Node<E>> findBy(E value) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
+        data.offer(this.parent);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
             if (el.eqValue(value)) {
